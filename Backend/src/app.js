853799a31a9b17a1863c5e308d2aad4fa267dcf8
Sushant Gupta <sushantg339@gmail.com>
2026-02-06 11:49:10 +0530

    fixed index.html problem

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -21,7 +21,7 @@ app.use('/api/auth' , authRoutes)
 app.use('/api/note', noteRoutes)
 
 app.get('/*splat', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'))
+    res.sendFile(path.join(__dirname, '../../Frontend/dist', 'index.html'))
 })
 
 module.exports = app
