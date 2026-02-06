8a31756ae3380c31fdc739dd85680292efd0f806
Sushant Gupta <sushantg339@gmail.com>
2026-02-06 21:40:40 +0530

    deploy =ment done

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,18 +1,18 @@
+require('dotenv').config()
 const express = require('express')
 const connectToDB = require('./db/db')
-require('dotenv').config()
 const cookieParser = require('cookie-parser')
 const authRoutes = require('./routes/auth.routes') 
 const noteRoutes = require('./routes/note.routes')
 const cors = require('cors')
-
+const path = require('path')
 const app = express()
 connectToDB()
 app.use(express.json())
 app.use(express.urlencoded({extended : true}))
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:5173", 
+    origin: true, 
     credentials: true
 }));
 
@@ -20,5 +20,11 @@ app.use(cors({
 
 app.use('/api/auth' , authRoutes)
 app.use('/api/note', noteRoutes)
+
+app.use(express.static(path.join(__dirname , "../dist")))
+
+app.use("/*splat" , (req,res)=>{
+    res.sendFile(path.join(__dirname , "../dist" , "index.html"))
+})
 
 module.exports = app
