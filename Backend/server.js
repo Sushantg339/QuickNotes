fe8094036dc19cd199e553f85f0a5e3bdc14dc08
Sushant Gupta <sushantg339@gmail.com>
2026-02-06 11:57:35 +0530

    port added

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,8 @@
-const app = require('./src/app')
+require('dotenv').config()
 
-app.listen(3000 , ()=>{
-    console.log('server is running on port 3000')
+
+const app = require('./src/app')
+const PORT = process.env.PORT || 3000
+app.listen(PORT , ()=>{
+    console.log(`server is running on port ${PORT}`)
 })
